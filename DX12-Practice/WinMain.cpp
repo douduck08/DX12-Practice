5c5f3c31600f046efac40f7872fd9eb65ba624ef
douduck08 <lu10082000@gmail.com>
2021-08-24 18:15:01 +0800

    show window

--- DX12-Practice/WinMain.cpp
+++ DX12-Practice/WinMain.cpp
@@ -34,11 +34,101 @@ ComPtr<ID3D12DescriptorHeap> g_RTVDescriptorHeap;
 UINT g_RTVDescriptorSize;
 UINT g_CurrentBackBufferIndex;
 
+// Synchronization Objects
+ComPtr<ID3D12Fence> g_Fence;
+uint64_t g_FenceValue = 0;
+uint64_t g_FrameFenceValues[g_NumFrames] = {};
+HANDLE g_FenceEvent;
+
+bool g_VSync = true;
+bool g_TearingSupported = false;
+bool g_Fullscreen = false;
+
+// Window callback function.
+LRESULT CALLBACK WindowProc(HWND, UINT, WPARAM, LPARAM);
+
+void RegisterWindowClass(HINSTANCE hInst, const wchar_t* windowClassName)
+{
+    WNDCLASSEXW windowClass = {};
+
+    windowClass.cbSize = sizeof(WNDCLASSEX);
+    windowClass.style = CS_HREDRAW | CS_VREDRAW;
+    windowClass.lpfnWndProc = &WindowProc;
+    windowClass.cbClsExtra = 0;
+    windowClass.cbWndExtra = 0;
+    windowClass.hInstance = hInst;
+    windowClass.hIcon = nullptr;
+    windowClass.hCursor = nullptr;
+    windowClass.hbrBackground = nullptr;
+    windowClass.lpszMenuName = nullptr;
+    windowClass.lpszClassName = windowClassName;
+    windowClass.hIconSm = nullptr;
+
+    static ATOM atom = ::RegisterClassExW(&windowClass);
+    assert(atom > 0);
+}
+
+HWND CreateWindowInstance(const wchar_t* windowClassName, HINSTANCE hInst, const wchar_t* windowTitle, uint32_t width, uint32_t height)
+{
+    RECT windowRect = { 0, 0, static_cast<LONG>(width), static_cast<LONG>(height) };
+    AdjustWindowRect(&windowRect, WS_OVERLAPPEDWINDOW, FALSE);
+
+    int windowWidth = windowRect.right - windowRect.left;
+    int windowHeight = windowRect.bottom - windowRect.top;
+
+    // Center the window within the screen. Clamp to 0, 0 for the top-left corner.
+    int screenWidth = ::GetSystemMetrics(SM_CXSCREEN);
+    int screenHeight = ::GetSystemMetrics(SM_CYSCREEN);
+    int windowX = std::max<int>(0, (screenWidth - windowWidth) / 2);
+    int windowY = std::max<int>(0, (screenHeight - windowHeight) / 2);
+
+    HWND hWnd = ::CreateWindowExW(
+        NULL,
+        windowClassName,
+        windowTitle,
+        WS_OVERLAPPEDWINDOW,
+        windowX,
+        windowY,
+        windowWidth,
+        windowHeight,
+        NULL,
+        NULL,
+        hInst,
+        nullptr
+    );
+
+    assert(hWnd && "Failed to create window");
+
+    return hWnd;
+}
+
 int CALLBACK WinMain(
 	_In_     HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
 	_In_     LPSTR     lpCmdLine,
 	_In_     int       nCmdShow)
 {
+    const wchar_t* windowClassName = L"DX12WindowClass";
+    const wchar_t* windowTitle = L"Learning DirectX 12";
+
+    RegisterWindowClass(hInstance, windowClassName);
+    g_hWnd = CreateWindowInstance(windowClassName, hInstance, windowTitle, g_ClientWidth, g_ClientHeight);
+    ShowWindow(g_hWnd, SW_SHOW);
+
+    MSG msg = {};
+    while (msg.message != WM_QUIT)
+    {
+        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
 	return 0;
 }
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
